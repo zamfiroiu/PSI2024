9d7e7e26cb384e5248baaaffd58bc3e9ab72bb5e
Alin.Zamfiroiu <Alin.Zamfiroiu@ALIN-ZAMFIROIU>
2024-02-27 08:17:59 +0200

    Add project files.

---
+++ PSI2024/PrimulCod.cpp
@@ -0,0 +1,16 @@
+
+
+//tip_returnat nume_functie([lista_params])
+#include<iostream>
+using namespace std;
+
+int main() {
+	printf("Text: %s\n", "Acesta este un text");
+	printf("Text: %s, Numar: %d\n", "Test", 78);
+
+	cout << "Acesta este un text"<<endl;
+	cout << 6<<endl;
+
+	return 0;
+}
+
